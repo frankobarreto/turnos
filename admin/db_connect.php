f969e6ba444211b3467293d6f4387d8a659008cb
frankobarreto <132008121+frankobarreto@users.noreply.github.com>
2023-04-27 14:48:15 -0500

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('146.190.169.175','efxhost_colas','LmAmpESZaxcN','efxhost_colas')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('146.190.169.175','efxhost_cola','EBvF0sPWAT8U','efxhost_cola')or die("Could not connect to mysql".mysqli_error($con));
